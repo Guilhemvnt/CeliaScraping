1377abb9bfeeaa13a1f359dbb062578aa6720261
guilhemvnt <vinet.guilhem@outlook.com>
2024-07-03 12:53:56 +0200

    [ADD] script to setup

---
+++ getSetUpFiles.sh
@@ -0,0 +1,21 @@
+wget http://10.42.0.1:8000/requirements.txt
+wget http://10.42.0.1:8000/package-list.txt
+wget http://10.42.0.1:8000/package-selection.txt
+
+# Mettre à jour la liste des paquets
+sudo apt update
+
+# Installer dselect
+sudo apt install dselect
+
+# Lire les sélections de paquets
+sudo dpkg --set-selections < package-selections.txt
+
+# Installer les paquets sélectionnés via dselect
+sudo dselect
+
+# Installer les bibliothèques Python
+pip install -r requirements.txt
+
+# Installer les paquets npm locaux
+npm install
