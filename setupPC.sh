7624d96c2aa1e06bd59ecd4719809d4960c2eb2d
guilhemvnt <vinet.guilhem@outlook.com>
2024-07-03 14:01:23 +0200

    [ADD] script to setup

--- setupPC.sh
+++ setupPC.sh
@@ -1,3 +1,9 @@
+export NVM_DIR="$HOME/.nvm"
+# This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# This loads nvm bash_completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 sudo apt-get update
 
 nvm install 20
