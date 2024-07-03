4ae5db2e54bf2ac552f6854649323244d9e487a4
guilhemvnt <vinet.guilhem@outlook.com>
2024-07-03 14:04:41 +0200

    [ADD] script to setup

--- setupPC.sh
+++ setupPC.sh
@@ -17,23 +17,23 @@ echo "install mermaid"
 npm install -g @mermaid-js/mermaid-cli
 
 echo "installing python3"
-sudo apt-get install python3
+sudo apt-get install -y  python3
 
 echo "installing python3-venv"
-sudo apt-get install python3-venv
+sudo apt-get install -y python3-venv
 
 echo "installing chromium"
 sudo apt-get 
 
 echo "installing pip3"
-sudo apt-get install python3-pip
+sudo apt-get install -y  python3-pip
 
 echo "installing chrome"
 sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 wget -O- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/linux_signing_key.pub
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78BD65473CB3BD13
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get install -y  google-chrome-stable
 
 touch .env
 
