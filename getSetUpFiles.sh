59a4ab401e98b7eb2e5f85f6793541b0545d18d7
guilhemvnt <vinet.guilhem@outlook.com>
2024-07-03 12:55:32 +0200

    [ADD] script to setup

--- getSetUpFiles.sh
+++ getSetUpFiles.sh
@@ -1,6 +1,6 @@
 wget http://10.42.0.1:8000/requirements.txt
 wget http://10.42.0.1:8000/package-list.txt
-wget http://10.42.0.1:8000/package-selection.txt
+wget http://10.42.0.1:8000/package-selections.txt
 
 # Mettre à jour la liste des paquets
 sudo apt update
